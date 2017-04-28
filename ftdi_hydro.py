2221a1c7232c90b63fb4dddd2276a3f73671f5c0
rsstdd <rosstod@gmail.com>
2017-04-28 16:32:25 -0700

    Fix sytax in loop

--- ftdi_hydro.py
+++ ftdi_hydro.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
 
     try:
         while True:
-            for i in devices
+            for i in range(len(devices))
                 print devices
                 dev = AtlasDevice(devices[int(index)])
                 dev.send_cmd("C,0") # turn off continuous mode
