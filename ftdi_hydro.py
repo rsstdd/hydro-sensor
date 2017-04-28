b16e4b52ff0d0bd16fca9adcf59ba23447ec67c7
rsstdd <rosstod@gmail.com>
2017-04-28 09:37:27 -0700

    Lint for syntax errors, explicitly read from single device/serial number

--- ftdi_hydro.py
+++ ftdi_hydro.py
@@ -15,29 +15,22 @@ class AtlasDevice(Device):
     def __init__(self, sn):
         Device.__init__(self, mode='t', device_id=sn)
 
-    def read_line(self):
+    def read_line(self, size=0):
         """
         Read the response from the Atlas Sensor
-        :return:
+        Modified to use the ezo line separator "\r"
         """
+        lsl = len('\r')
         line_buffer = []
-        try:
-            start_time = time.time()
-            while True:
-
-                # read bytes until Carriage Return is received.
-                next_char = self.read(1)
-                if next_char == "\r":  # response of sensor always ends with CR.
-                    break
-                line_buffer.append(next_char)
-
-                if time.time() - start_time > 1.0:
-                    line_buffer = ''
-                    break
-            return ''.join(line_buffer)
-
-        except FtdiError:
-            return ''
+        while True:
+            next_char = self.read(1)
+            if next_char == '' or (size > 0 and len(line_buffer) > size):
+                break
+            line_buffer.append(next_char)
+            if (len(line_buffer) >= lsl and
+                    line_buffer[-lsl:] == list('\r')):
+                break
+        return ''.join(line_buffer)
 
     def read_lines(self):
         """
@@ -84,7 +77,6 @@ def log_sensor_readings(all_curr_readings):
                     # print lines[i]
                     if lines[i][0] != '*':
                         print "Response: ", lines[i]
-                time.sleep(delaytime)
         # catches the ctrl-c command, which breaks the loop above
         except KeyboardInterrupt:
             print("Continuous polling stopped")
@@ -109,36 +101,22 @@ def read_sensors():
     all_curr_readings = []
     ref_temp = 25
 
+    print (sensors.items())
+
     for key, value in sensors.items():
+        print key, value
         # Get the readings from any Atlas Scientific temperature sensors to use as ref_temp
-        print " %s " % key
-        dev = AtlasDevice(value["serial_number"])
-        dev.send_cmd("T," + str(ref_temp))
-
-# Get the readings from any Atlas Scientific Elec Conductivity sensors
-
-        if value["sensor_type"] == "atlas_scientific_ph":
-            dev = AtlasDevice(value["serial_number"])
-            dev.send_cmd("R")
-            sensor_reading = dev.read_line()
-
-            # (round(((float(dev.read_line())) *
-                            #   value["ppm_multiplier"]), value["accuracy"]))
-
-# Get the readings from any other Atlas Scientific sensors
-
-        else:
-            dev = AtlasDevice(value["serial_number"])
-            dev.send_cmd("R")
-            sensor_reading = round(float(dev.read_line()),
-                                   value["accuracy"])
-            all_curr_readings.append([value["name"], sensor_reading])
+        dev = AtlasDevice(value["DJ00RUFM"])
+        dev.send_cmd("R")
+        sensor_reading = dev.read_line()
+        all_curr_readings.append([value["atlas_scientific_ph"], sensor_reading])
 
     log_sensor_readings(all_curr_readings)
 
     return
 
 sensors = OrderedDict([("atlas_sensor_ph", {"sensor_type": "atlas_scientific_ph", "name": "ph", "is_connected": True, "is_ref": False, "serial_number": 'DJ00RUFM', "accuracy": 2})])
+
 loops = 0
 
 while True:  # Repeat the code indefinitely
@@ -148,7 +126,8 @@ while True:  # Repeat the code indefinitely
 
         read_sensors()
 
-        print "%s" % loops
+        print loops
+        print sensors
 
     loops += 1
     time.sleep(1)
