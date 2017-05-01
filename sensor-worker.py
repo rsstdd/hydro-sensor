3dd8e20bf8d857307ed27d56318a93bc3f8b0c67
rsstdd <rosstod@gmail.com>
2017-05-01 14:54:01 -0700

    Test loop

--- sensor-worker.py
+++ sensor-worker.py
@@ -4,16 +4,15 @@
 import os, sys
 import numpy
 import json
-import gdrive_util as gu
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-import pymongo
+# import gdrive_util as gu
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+# import pymongo
 from datetime import datetime, timedelta
 import ftdi_hydro
 
-gauth = GoogleAuth()
-gauth.CommandLineAuth()
-drive = GoogleDrive(gauth)
-
 while 1:
-    print ftdi_hydro.get_sensor_data()
+    sensor-data=ftdi_hydro.get_sensor_data()
+
+    for item in sensorData:
+        print item
