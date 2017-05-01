4150d7288ca697f59623705d21fd62f866ce9d87
rsstdd <rosstod@gmail.com>
2017-05-01 16:36:49 -0700

    add return

--- ftdi_hydro.py
+++ ftdi_hydro.py
@@ -150,14 +150,14 @@ sensors = OrderedDict([("atlas_sensor_1", {  # Atlas Scientific Temp Sensor
 def get_reading():
     read_sensors()
 
-
-loops = 0
-
-while True:  # Repeat the code indefinitely
-
-    if loops == 300:
-        loops = 0
-
-        read_sensors()
-
-    loops += 1
+# 
+# loops = 0
+#
+# while True:  # Repeat the code indefinitely
+#
+#     if loops == 300:
+#         loops = 0
+#
+#         read_sensors()
+#
+#     loops += 1
