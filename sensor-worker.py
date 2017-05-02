ee44ae16bc3403dfc5ea530d02f823cc47a79098
rsstdd <rosstod@gmail.com>
2017-05-02 12:09:32 -0700

    timestamp

--- sensor-worker.py
+++ sensor-worker.py
@@ -60,11 +60,13 @@ def mongoize(type,jsonPackage):
     #     print e
 
 if hydroData is not None:
+    timestamp = datetime.datetime.now()
+
     for data in hydroData:
         jsonPackage={
         'sensor_num': hydroData[0]['serial_number'],
         'hostname' : gethostname(),
-        'timestamp': timestamp,
+        'timestamp': time.time,
         'sensor_version':'1.00',
         'sensor_group': '',
         'role': 'hydro_sensor',
