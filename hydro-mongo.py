12c6d866e56bc6dc19e2044891fec1d6ea04928f
rsstdd <rosstod@gmail.com>
2017-05-02 14:34:46 -0700

    timestamp

--- hydro-mongo.py
+++ hydro-mongo.py
@@ -11,55 +11,6 @@ import ftdi_hydro
 
 hydroData=ftdi_hydro.read_sensors()
 
-def postAPI(url, payload):
-    try:
-        r = requests.post(url, data = payload)
-        assert r.status_code == 201, "%r %r != 201" % (r.url,r.status_code)
-        print "sent", r.url
-    except Exception as e:
-        print "yocto-mongo.py FAILED to send to", e
-
-def mongoize(type,jsonPackage):
-#   print jsonPackage
-    # filename="/var/local/thoth.id"
-    deviceData = {}
-    deviceData['room']="undefined"
-    deviceData['role']="undefined"
-
-    # with open(filename,"r") as file:
-    #     deviceData=json.load(file)
-    #     file.close()
-
-    # client = MongoClient('10.9.0.1')
-    # db=client.solstice
-    # collection=db[type]
-    jsonPackage['type'] = type
-    jsonPackage['room'] = deviceData['room']
-    jsonPackage['role'] = deviceData['role']
-    sensorRecord = {"sensordata":jsonPackage}
-
-    # try :
-    #     # record_id2=db.sensordata.insert_one(sensorRecord)
-    # except:
-    #     with open('~thoth/sensordata.txt','w') as outfile:
-    #         json.dump(jsonPackage, outfile)
-    #
-    # record_id=db[type].insert_one(jsonPackage).inserted_id
-
-    # print record_id
-    # print record_id2
-
-    print sensorRecord
-
-    #send to heroku
-
-    # try:
-
-        # r = requests.post('https://luna-api.herokuapp.com/sensordata', data = jsonPackage)
-        # r2 = requests.post('https://luna-api-staging.herokuapp.com/sensordata', data = jsonPackage)
-    # except Exception as e:
-    #     print e
-
 if hydroData is not None:
     # timestamp = datetime.datetime.now()
 
