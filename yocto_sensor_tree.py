1ad9b1d26ba387531830f2b39c106f4206975af0
rsstdd <rosstod@gmail.com>
2017-05-03 16:59:57 -0700

    Yocto Sensor Data

--- yocto_sensor_tree.py
+++ yocto_sensor_tree.py
@@ -29,11 +29,9 @@ else:
         humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
         tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
 
-
         lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
         humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
         tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
-
 
         lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
         humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
@@ -77,64 +75,3 @@ else:
         )
 
         time.sleep(10)
-
-
-    # if lightT.isOnline() and humidT.isOnline() is True:
-    #     i = 1
-    #
-    #     for i in range(1, 6):
-    #         print (
-    #             '%2.2f' % tempT.get_currentValue() + "deg F   "
-    #             + "%2.1f" % humidT.get_currentValue() + "%  "
-    #             + str(int(lightT.get_currentValue())/82) + ' PAR'
-    #         )
-    #
-    #         i = i + 1
-    #         time.sleep(1)
-    # else:
-    #     print('No devices found!')
-    #
-    #
-    # if lightTM.isOnline() and humidTM.isOnline() is True:
-    #     i = 1
-    #
-    #     for i in range(1, 6):
-    #         print(
-    #             '%2.2f' % tempTM.get_currentValue() + "deg F   "
-    #             + "%2.1f" % humidTM.get_currentValue() + "%  "
-    #             + str(int(lightTM.get_currentValue())/82) + ' PAR'
-    #         )
-    #
-    #         i = i + 1
-    #         time.sleep(1)
-    # else:
-    #     print('No devices found!')
-    #
-    # if lightML.isOnline() and humidML.isOnline() is True:
-    #     i = 1
-    #
-    #     for i in range(1, 6):
-    #         print(
-    #             '%2.2f' % tempML.get_currentValue() + "deg F   "
-    #             + "%2.1f" % humidML.get_currentValue() + "%  "
-    #             + str(int(lightML.get_currentValue()) / 82) + ' PAR'
-    #         )
-    #
-    #         i = i + 1
-    #         time.sleep(1)
-    #
-    # else:
-    #     print('No devices found!')
-    #
-    # if lightL.isOnline() and humidL.isOnline() is True:
-    #         print(
-    #             '%2.2f' % tempL.get_currentValue() + "deg F   "
-    #             + "%2.1f" % humidL.get_currentValue() + "%  "
-    #             + str(int(lightL.get_currentValue()) / 82) + ' PAR'
-    #         )
-    # else:
-    #     print('No devices found!')
-    #     print('TEST COMPLETE')
-    #
-    #
-    # time.sleep(10)
