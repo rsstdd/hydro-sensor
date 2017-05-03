63b6841f5d3eaedac67d53babf1530ee0e5c3ab3
rsstdd <rosstod@gmail.com>
2017-05-03 09:50:13 -0700

    Get Sensor data

--- sources/atlas_hydro.py
+++ sources/atlas_hydro.py
@@ -52,8 +52,8 @@ def log_sensor_readings(all_curr_readings):
     for reading in all_curr_readings:
         try:
             print ''
-            # print reading
-            print '---------'
+            print reading
+            print ''
         except:
             pass
 
