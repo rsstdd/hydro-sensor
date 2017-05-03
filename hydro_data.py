2de59bf1ae4567d8d27769e2325ddab84b559741
rsstdd <rosstod@gmail.com>
2017-05-03 15:42:12 -0700

    Yocto Sensor Data

--- hydro_data.py
+++ hydro_data.py
@@ -18,37 +18,70 @@ def format_data():
     if hydroData is not None:
         for sensor in hydroData:
             if sensor["sensor_type"] == "atlas_scientific_ec":
-                hydrojson.append({
+                # hydrojson.append({
+                    # "sensor_num": sensor["serial_number"],
+                    # "type": "ec",
+                    # "role": sensor["sensor_type"],
+                    # "ec": sensor["ec"],
+                # })
+                ec_data = {
                     "sensor_num": sensor["serial_number"],
                     "type": "ec",
                     "role": sensor["sensor_type"],
                     "ec": sensor["ec"],
-                })
+                }
 
-                hydrojson.append({
+                dispatch_sensor_data('HYDRO_EC', ec_data)
+
+                # hydrojson.append({
+                #     "sensor_num": sensor["serial_number"],
+                #     "role": sensor["sensor_type"],
+                #     "type": "ppm",
+                #     "ppm": sensor["ppm"]
+                # })
+
+                ppm_data = {
                     "sensor_num": sensor["serial_number"],
                     "role": sensor["sensor_type"],
                     "type": "ppm",
                     "ppm": sensor["ppm"]
-                })
+                }
+
+                dispatch_sensor_data('HYDRO_PPM', ec_data)
+
 
             if sensor["sensor_type"] == "atlas_scientific_temp":
-                hydrojson.append({
+                # hydrojson.append({
+                #     "sensor_num": sensor["serial_number"],
+                #     "role": sensor["sensor_type"],
+                #     "type": "hydro-temp",
+                #     "hydro-temp": sensor["sensor_reading"]
+                # })
+
+                temp_data = {
                     "sensor_num": sensor["serial_number"],
                     "role": sensor["sensor_type"],
                     "type": "hydro-temp",
                     "hydro-temp": sensor["sensor_reading"]
-                })
+                }
+
+                dispatch_sensor_data('HYDRO_TEMP', temp_data)
 
             if sensor["sensor_type"] == "atlas_scientific_ph":
-                hydrojson.append({
+                # hydrojson.append({
+                #     "sensor_num": sensor["serial_number"],
+                #     "role": sensor["sensor_type"],
+                #     "type": "hydro-ph",
+                #     "hydro-ph": sensor["sensor_reading"]
+                # })
+
+                temp_data = {
                     "sensor_num": sensor["serial_number"],
                     "role": sensor["sensor_type"],
                     "type": "hydro-ph",
                     "hydro-ph": sensor["sensor_reading"]
-                })
+                }
 
-    return hydrojson
-
+                dispatch_sensor_data('HYDRO_TEMP', temp_data)
 
 format_data()
