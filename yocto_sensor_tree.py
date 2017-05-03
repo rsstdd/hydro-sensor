98e50e8cf29afdb04bb020041c28bb5f7cd654cb
rsstdd <rosstod@gmail.com>
2017-05-03 16:29:24 -0700

    Yocto Sensor Data

--- yocto_sensor_tree.py
+++ yocto_sensor_tree.py
@@ -19,87 +19,87 @@ if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
     sys.exit("init error" + errmsg.value)
 
 
-if target == 'all':
-    # retreive any humidity sensor
 
-    print('TOP LEVEL')
-    lightT = YLightSensor.FindLightSensor("LIGHTMK3-853DA.lightSensor")
-    humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
-    tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
+# retreive any humidity sensor
 
-    if lightT.isOnline() and humidT.isOnline() is True:
-        i = 1
+print('TOP LEVEL')
+lightT = YLightSensor.FindLightSensor("LIGHTMK3-853DA.lightSensor")
+humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
+tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
 
-        for i in range(1, 6):
-            print (
-                '%2.2f' % tempT.get_currentValue() + "deg F   "
-                + "%2.1f" % humidT.get_currentValue() + "%  "
-                + str(int(lightT.get_currentValue())/82) + ' PAR'
-            )
+if lightT.isOnline() and humidT.isOnline() is True:
+    i = 1
 
-            i = i + 1
-            time.sleep(1)
-    else:
-        print('No devices found!')
-        print('TOP MID LEVEL')
+    for i in range(1, 6):
+        print (
+            '%2.2f' % tempT.get_currentValue() + "deg F   "
+            + "%2.1f" % humidT.get_currentValue() + "%  "
+            + str(int(lightT.get_currentValue())/82) + ' PAR'
+        )
 
-        lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
-        humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
-        tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
+        i = i + 1
+        time.sleep(1)
+else:
+    print('No devices found!')
+    print('TOP MID LEVEL')
 
-    if lightTM.isOnline() and humidTM.isOnline() is True:
-        i = 1
+    lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
+    humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
+    tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
 
-        for i in range(1, 6):
-            print(
-                '%2.2f' % tempTM.get_currentValue() + "deg F   "
-                + "%2.1f" % humidTM.get_currentValue() + "%  "
-                + str(int(lightTM.get_currentValue())/82) + ' PAR'
-            )
+if lightTM.isOnline() and humidTM.isOnline() is True:
+    i = 1
 
-            i = i + 1
-            time.sleep(1)
-    else:
-        print('No devices found!')
-        print('MID LOW LEVEL')
+    for i in range(1, 6):
+        print(
+            '%2.2f' % tempTM.get_currentValue() + "deg F   "
+            + "%2.1f" % humidTM.get_currentValue() + "%  "
+            + str(int(lightTM.get_currentValue())/82) + ' PAR'
+        )
 
-        lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
-        humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
-        tempML = YTemperature.FindTemperature('METEOMK1-85165.temperature')
+        i = i + 1
+        time.sleep(1)
+else:
+    print('No devices found!')
+    print('MID LOW LEVEL')
 
-    if lightML.isOnline() and humidML.isOnline() is True:
-        i = 1
+    lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
+    humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
+    tempML = YTemperature.FindTemperature('METEOMK1-85165.temperature')
 
-        for i in range(1, 6):
-            print(
-                '%2.2f' % tempML.get_currentValue() + "deg F   "
-                + "%2.1f" % humidML.get_currentValue() + "%  "
-                + str(int(lightML.get_currentValue()) / 82) + ' PAR'
-            )
+if lightML.isOnline() and humidML.isOnline() is True:
+    i = 1
 
-            i = i + 1
-            time.sleep(1)
+    for i in range(1, 6):
+        print(
+            '%2.2f' % tempML.get_currentValue() + "deg F   "
+            + "%2.1f" % humidML.get_currentValue() + "%  "
+            + str(int(lightML.get_currentValue()) / 82) + ' PAR'
+        )
 
-    else:
-        print('No devices found!')
-        print('LOW LEVEL')
+        i = i + 1
+        time.sleep(1)
 
-        lightL = YLightSensor.FindLightSensor('LIGHTMK3-85205.lightSensor')
-        humidL = YHumidity.FindHumidity('METEOMK1-7FA23.humidity')
-        tempL = YTemperature.FindTemperature('METEOMK1-7FA23.temperature')
+else:
+    print('No devices found!')
+    print('LOW LEVEL')
 
-    if lightL.isOnline() and humidL.isOnline() is True:
-        i = 1
+    lightL = YLightSensor.FindLightSensor('LIGHTMK3-85205.lightSensor')
+    humidL = YHumidity.FindHumidity('METEOMK1-7FA23.humidity')
+    tempL = YTemperature.FindTemperature('METEOMK1-7FA23.temperature')
 
-        for i in range(1, 6):
-            print(
-                '%2.2f' % tempL.get_currentValue() + "deg F   "
-                + "%2.1f" % humidL.get_currentValue() + "%  "
-                + str(int(lightL.get_currentValue()) / 82) + ' PAR'
-            )
+if lightL.isOnline() and humidL.isOnline() is True:
+    i = 1
 
-            i = i + 1
-            time.sleep(1)
-    else:
-        print('No devices found!')
-        print('TEST COMPLETE')
+    for i in range(1, 6):
+        print(
+            '%2.2f' % tempL.get_currentValue() + "deg F   "
+            + "%2.1f" % humidL.get_currentValue() + "%  "
+            + str(int(lightL.get_currentValue()) / 82) + ' PAR'
+        )
+
+        i = i + 1
+        time.sleep(1)
+else:
+    print('No devices found!')
+    print('TEST COMPLETE')
