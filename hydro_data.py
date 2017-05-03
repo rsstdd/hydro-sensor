eb4a222ee52a09daecef5276436c4a6df85c493b
rsstdd <rosstod@gmail.com>
2017-05-03 10:12:46 -0700

    Get Sensor data

--- hydro_data.py
+++ hydro_data.py
@@ -17,15 +17,17 @@ def format_data():
     if hydroData is not None:
 
         for data in hydroData:
-            jsonPackage = {
-                'sensor_num': data['serial_number'],
-                'hostname': gethostname(),
-                'timestamp': 'Should-be-timestamp',
-                'sensor_version': '1.00',
-                'sensor_group': data['name'],
-                'role': data['sensor_type'],
-                'type': data['sensor_type']
-            }
+
+            print data
+            # jsonPackage = {
+            #     'sensor_num': data['serial_number'],
+            #     'hostname': gethostname(),
+            #     'timestamp': 'Should-be-timestamp',
+            #     'sensor_version': '1.00',
+            #     'sensor_group': data['name'],
+            #     'role': data['sensor_type'],
+            #     'type': data['sensor_type']
+            # }
 
         hydrojson.append(jsonPackage)
 
