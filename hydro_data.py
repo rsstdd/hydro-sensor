99166444c938928942546ae83beec7cb81992a01
rsstdd <rosstod@gmail.com>
2017-05-03 10:02:48 -0700

    Get Sensor data

--- hydro_data.py
+++ hydro_data.py
@@ -6,34 +6,36 @@ from datetime import timedelta, date
 import os, sys
 # add ../sources to the PYTHONPATH
 sys.path.append(os.path.join('./sources'))
-
 import atlas_hydro
 
+hydroData = atlas_hydro.get_sensor_data()
 
-def format_data():
-    hydroData = atlas_hydro.get_sensor_data()
-    hydrojson = []
-
-    if hydroData is not None:
-        try:
-            for data in hydroData:
-                jsonPackage = {
-                    'sensor_num': data['serial_number'],
-                    'hostname': gethostname(),
-                    'timestamp': 'Should-be-timestamp',
-                    'sensor_version': '1.00',
-                    'sensor_group': data['name'],
-                    'role': data['sensor_type'],
-                    'type': data['sensor_type']
-                }
-
-            hydrojson.append(jsonPackage)
-
-            print jsonPackage
-        except:
-            print "No hydrodata"
-
-    # return hydrojson
+print hydroData
 
 
-format_data()
+# def format_data():
+#     hydrojson = []
+#
+#     if hydroData is not None:
+#         try:
+#             for data in hydroData:
+#                 jsonPackage = {
+#                     'sensor_num': data['serial_number'],
+#                     'hostname': gethostname(),
+#                     'timestamp': 'Should-be-timestamp',
+#                     'sensor_version': '1.00',
+#                     'sensor_group': data['name'],
+#                     'role': data['sensor_type'],
+#                     'type': data['sensor_type']
+#                 }
+#
+#             hydrojson.append(jsonPackage)
+#
+#             print jsonPackage
+#         except:
+#             print "No hydrodata"
+#
+#     # return hydrojson
+#
+#
+# format_data()
