56d95a7110984ab23cbf46d1fd1a2e8d2d40432c
rsstdd <rosstod@gmail.com>
2017-05-04 12:08:01 -0700

    Quick lint

--- sensor_worker.py
+++ sensor_worker.py
@@ -5,7 +5,7 @@ import os
 import sys
 sys.path.append(os.path.join("./sources"))
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from socket import gethostname
 from datetime import timedelta, date
 import json
