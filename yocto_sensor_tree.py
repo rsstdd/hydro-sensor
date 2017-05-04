7f2ae5fe88838f3fc3bb48a73192d734403a9831
rsstdd <rosstod@gmail.com>
2017-05-04 09:55:15 -0700

    init commit

--- yocto_sensor_tree.py
+++ yocto_sensor_tree.py
@@ -18,70 +18,64 @@ errmsg = YRefParam()
 if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
     sys.exit("init error" + errmsg.value)
 
-else:
-    module = YModule.FirstModule()
+# Top - lvl 1
+# ------------
+lightT = YLightSensor.FindLightSensor("LIGHTMK3-853DA.lightSensor")
+humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
+tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
 
-    while module is not None:
-        print(module.get_serialNumber() + ' (' + module.get_productName() + ')')
+# Top Middle - Lvl 2
+# -------------------
+lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
+humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
+tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
 
+# Middle - Lvl 3
+# ---------------
+lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
+humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
+tempML = YTemperature.FindTemperature('METEOMK1-85165.temperature')
 
-        # Top - lvl 1
-        # ------------
-        lightT = YLightSensor.FindLightSensor("LIGHTMK3-853DA.lightSensor")
-        humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
-        tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
+# Lower - Lvl 4
+# --------------
+lightL = YLightSensor.FindLightSensor('LIGHTMK3-85205.lightSensor')
+humidL = YHumidity.FindHumidity('METEOMK1-7FA23.humidity')
+tempL = YTemperature.FindTemperature('METEOMK1-7FA23.temperature')
 
-        # Top Middle - Lvl 2
-        # -------------------
-        lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
-        humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
-        tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
+sensors = [lightTM, humidTM, tempTM, lightML, humidML, tempML, lightL, tempL, humidL]
 
-        # Middle - Lvl 3
-        # ---------------
-        lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
-        humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
-        tempML = YTemperature.FindTemperature('METEOMK1-85165.temperature')
+for sensor in sensors:
+    print sensor..get_currentValue()
 
-        # Lower - Lvl 4
-        # --------------
-        lightL = YLightSensor.FindLightSensor('LIGHTMK3-85205.lightSensor')
-        humidL = YHumidity.FindHumidity('METEOMK1-7FA23.humidity')
-        tempL = YTemperature.FindTemperature('METEOMK1-7FA23.temperature')
-
-
-
-        print('TOP LEVEL')
-        print (
-            '%2.2f' % tempT.get_currentValue() + "deg F   "
-            + "%2.1f" % humidT.get_currentValue() + "%  "
-            + str(int(lightT.get_currentValue())/82) + ' PAR'
-        )
-
-        print('TOP MID LEVEL')
-        print (
-            '%2.2f' % tempTM.get_currentValue() + "deg F   "
-            + "%2.1f" % humidTM.get_currentValue() + "%  "
-            + str(int(lightTM.get_currentValue())/82) + ' PAR'
-        )
-
-        print('MID LOW LEVEL')
-
-        print (
-            '%2.2f' % tempML.get_currentValue() + "deg F   "
-            + "%2.1f" % humidML.get_currentValue() + "%  "
-            + str(int(lightML.get_currentValue())/82) + ' PAR'
-        )
-
-        print('LOW LEVEL')
-
-        print (
-            '%2.2f' % tempL.get_currentValue() + "deg F   "
-            + "%2.1f" % humidL.get_currentValue() + "%  "
-            + str(int(lightL.get_currentValue())/82) + ' PAR'
-        )
-
-        print '----------------'
-        print ''
-
-        time.sleep(10)
+# print('TOP LEVEL')
+# print (
+#     '%2.2f' % tempT.get_currentValue() + "deg F   "
+#     + "%2.1f" % humidT.get_currentValue() + "%  "
+#     + str(int(lightT.get_currentValue())/82) + ' PAR'
+# )
+#
+# print('TOP MID LEVEL')
+# print (
+#     '%2.2f' % tempTM.get_currentValue() + "deg F   "
+#     + "%2.1f" % humidTM.get_currentValue() + "%  "
+#     + str(int(lightTM.get_currentValue())/82) + ' PAR'
+# )
+#
+# print('MID LOW LEVEL')
+#
+# print (
+#     '%2.2f' % tempML.get_currentValue() + "deg F   "
+#     + "%2.1f" % humidML.get_currentValue() + "%  "
+#     + str(int(lightML.get_currentValue())/82) + ' PAR'
+# )
+#
+# print('LOW LEVEL')
+#
+# print (
+#     '%2.2f' % tempL.get_currentValue() + "deg F   "
+#     + "%2.1f" % humidL.get_currentValue() + "%  "
+#     + str(int(lightL.get_currentValue())/82) + ' PAR'
+# )
+#
+# print '----------------'
+# print ''
