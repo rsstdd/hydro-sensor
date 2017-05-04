9a24f2e3df95beaaccb264c19d1e5e2f3fead32d
rsstdd <rosstod@gmail.com>
2017-05-04 08:51:14 -0700

    Yocto Sensor Data

--- yocto_sensor_tree.py
+++ yocto_sensor_tree.py
@@ -3,6 +3,7 @@
 import os
 import sys
 sys.path.append(os.path.join('./sources'))
+from sensor_worker import dispatch_sensor_data
 
 from yocto_api import *
 from yocto_humidity import *
@@ -11,8 +12,8 @@ from yocto_pressure import *
 from yocto_lightsensor import *
 import time
 
-errmsg = YRefParam()
 
+errmsg = YRefParam()
 
 if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
     sys.exit("init error" + errmsg.value)
@@ -24,20 +25,26 @@ else:
         print(module.get_serialNumber() + ' (' + module.get_productName() + ')')
 
 
-
+        # Top - lvl 1
+        # ------------
         lightT = YLightSensor.FindLightSensor("LIGHTMK3-853DA.lightSensor")
         humidT = YHumidity.FindHumidity('METEOMK1-7FB03.humidity')
         tempT = YTemperature.FindTemperature('METEOMK1-7FB03.temperature')
 
+        # Top Middle - Lvl 2
+        # -------------------
         lightTM = YLightSensor.FindLightSensor('LIGHTMK3-7CCBA.lightSensor')
         humidTM = YHumidity.FindHumidity('METEOMK1-850EF.humidity')
         tempTM = YTemperature.FindTemperature('METEOMK1-850EF.temperature')
 
+        # Middle - Lvl 3
+        # ---------------
         lightML = YLightSensor.FindLightSensor('LIGHTMK3-7CAB6.lightSensor')
         humidML = YHumidity.FindHumidity('METEOMK1-85165.humidity')
         tempML = YTemperature.FindTemperature('METEOMK1-85165.temperature')
 
-
+        # Lower - Lvl 4
+        # --------------
         lightL = YLightSensor.FindLightSensor('LIGHTMK3-85205.lightSensor')
         humidL = YHumidity.FindHumidity('METEOMK1-7FA23.humidity')
         tempL = YTemperature.FindTemperature('METEOMK1-7FA23.temperature')
@@ -73,5 +80,8 @@ else:
             + "%2.1f" % humidL.get_currentValue() + "%  "
             + str(int(lightL.get_currentValue())/82) + ' PAR'
         )
+
+        print '----------------'
+        print ''
 
         time.sleep(10)
