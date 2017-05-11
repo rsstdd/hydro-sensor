1a71be6f92dfb7938e71442fe34a5e1d8cfd7886
rsstdd <rosstod@gmail.com>
2017-05-11 16:59:07 -0700

    Resolve DB issue

--- sensor_worker.py
+++ sensor_worker.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 import time
 import os
-import sys
-sys.path.append(os.path.join("./sources"))
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from socket import gethostname
 import datetime
 import json
@@ -14,44 +12,45 @@ import requests
 
 def dispatch_sensor_data(jsonPackage):
     timestamp = str(datetime.datetime.now())
-    filename = "/var/local/thoth.id"
+    thoth = "/var/local/thoth.id"
     deviceData = {}
     deviceData['room'] = "Undefined"
     deviceData['role'] = "Undefined"
 
     try:
-        with open(filename, 'r') as file:
+        with open(thoth, 'r') as file:
             deviceData = json.load(file)
             file.close()
     except Exception as e:
         print e
 
-    client = MongoClient('10.9.0.1')
-    db = client.solstice
-    collection = db[type]
+    # client = MongoClient('10.9.0.1')
+    # db = client.solstice
+    # collection = db[type]
 
     jsonPackage['room'] = deviceData['locaton']['room']
     jsonPackage['role'] = deviceData['device']['role']
     jsonPackage["timestamp"] = timestamp
     jsonPackage["sensor_version"] = "1.00"
     jsonPackage["sensor_group"] = "Production"
-
+    #
     sensorRecord = {"sensordata": jsonPackage}
 
-    print sensorRecord
-
-    try:
-        record_id2 = db.sensordata.insert_one(sensorRecord)
-    except:
-        with open('~thoth/sensordata.txt', 'w') as outfile:
-            json.dump(jsonPackage, outfile)
-
-    record_id=db[type].insert_one(jsonPackage).inserted_id
 
     print sensorRecord
 
-    try:
-        r = requests.post('https://luna-api.herokuapp.com/sensordata', data = jsonPackage)
-        r2 = requests.post('https://luna-api-staging.herokuapp.com/sensordata', data = jsonPackage)
-    except Exception as e:
-        print e
+    # try:
+    #     record_id2 = db.sensordata.insert_one(sensorRecord)
+    # except:
+    #     with open('~thoth/sensordata.txt', 'w') as outfile:
+    #         json.dump(jsonPackage, outfile)
+    #
+    # record_id=db[type].insert_one(jsonPackage).inserted_id
+    #
+    # print sensorRecord
+    #
+    # try:
+    #     r = requests.post('https://luna-api.herokuapp.com/sensordata', data = jsonPackage)
+    #     r2 = requests.post('https://luna-api-staging.herokuapp.com/sensordata', data = jsonPackage)
+    # except Exception as e:
+    #     print e
