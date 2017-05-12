f7b47cc8a8a67e06d2038a435b2b81775d8d34df
rsstdd <rosstod@gmail.com>
2017-05-12 09:50:04 -0700

    Troubleshoot Mongo Err

--- sensor_worker.py
+++ sensor_worker.py
@@ -3,12 +3,18 @@
 import time
 import os
 
-# from pymongo import MongoClient
 from socket import gethostname
 import datetime
 import json
 import requests
 
+def postAPI(url, payload):
+    try:
+        r = requests.post(url, data = payload)
+        assert r.status_code == 201, "%r %r != 201" % (r.url, r.status_code)
+        print "sent", r.url
+    except Exception as e:
+        print "yocto-mongo.py FAILED to send to", e
 
 def dispatch_sensor_data(jsonPackage):
     timestamp = str(datetime.datetime.now())
@@ -24,33 +30,23 @@ def dispatch_sensor_data(jsonPackage):
     except Exception as e:
         print e
 
-    # client = MongoClient('10.9.0.1')
-    # db = client.solstice
-    # collection = db[type]
+    client = MongoClient('10.9.0.1')
+    db = client.solstice
+    collection = db[type]
 
     jsonPackage['room'] = deviceData['locaton']['room']
     jsonPackage['role'] = deviceData['device']['role']
     jsonPackage["timestamp"] = timestamp
     jsonPackage["sensor_version"] = "1.00"
-    jsonPackage["sensor_group"] = "Production"
-    #
+    jsonPackage["sensor_group"] = "test"
+
     sensorRecord = {"sensordata": jsonPackage}
 
+    print jsonPackage
 
-    print sensorRecord
-
-    # try:
-    #     record_id2 = db.sensordata.insert_one(sensorRecord)
-    # except:
-    #     with open('~thoth/sensordata.txt', 'w') as outfile:
-    #         json.dump(jsonPackage, outfile)
-    #
-    # record_id=db[type].insert_one(jsonPackage).inserted_id
-    #
-    # print sensorRecord
-    #
-    # try:
-    #     r = requests.post('https://luna-api.herokuapp.com/sensordata', data = jsonPackage)
-    #     r2 = requests.post('https://luna-api-staging.herokuapp.com/sensordata', data = jsonPackage)
-    # except Exception as e:
-    #     print e
+    try:
+        # postAPI('https://luna-api.herokuapp.com/sensordata', jsonPackage)
+        # postAPI('https://luna-api-staging.herokuapp.com/sensordata', jsonPackage)
+    except:
+        with open('~thoth/sensordata.txt', 'w') as outfile:
+            json.dump(jsonPackage, outfile)
