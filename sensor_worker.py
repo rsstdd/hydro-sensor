64199cfd6f60a7b9a47a879ac4e9ef4a7e97215d
rsstdd <rosstod@gmail.com>
2017-05-22 14:38:31 -0700

    Troubleshoot heroku err

--- sensor_worker.py
+++ sensor_worker.py
@@ -64,13 +64,13 @@ def dispatch_sensor_data(dataPackage):
 
     #  Mongo
 
-    json = json.dupm(sensorRecord)
+    sensorPayload = json.dupm(sensorRecord)
 
     try:
         client = MongoClient('10.9.0.1:27017')
         db = client.solstice
         collection = db[type]
-        record_id2 = db.sensordata.insert_one(json)
+        record_id2 = db.sensordata.insert_one(sensorPayload)
         client.close()
         print "mongo sent"
 
