691c33e2bd85384233130575be35417ca98c8362
rsstdd <rosstod@gmail.com>
2017-05-23 17:22:27 -0700

    Mongo

--- hydro_data.py
+++ hydro_data.py
@@ -13,49 +13,48 @@ from sensor_worker import dispatch_sensor_data
 hydroData = atlas_hydro.read_sensors() # should this be in the loop?
 
 def format_data():
-    if hydroData:
-        for sensor in hydroData:
+	if hydroData:
+		for sensor in hydroData:
 
-            if sensor["sensor_type"] == "atlas_scientific_ec":
-                ec_data = {
-                    "sensor_num": sensor["serial_number"],
-                    "type": "ec",
-                    "role": sensor["sensor_type"],
-                    "ec": sensor["ec"],
-                }
+			if sensor["sensor_type"] == "atlas_scientific_ec":
+				ec_data = {
+					"sensor_num": sensor["serial_number"],
+					"type": "ec",
+					"role": sensor["sensor_type"],
+					"ec": sensor["ec"],
+				}
 
-                dispatch_sensor_data(ec_data)
+				dispatch_sensor_data(ec_data)
 
-                ppm_data = {
-                    "sensor_num": sensor["serial_number"],
-                    "role": sensor["sensor_type"],
-                    "type": "ppm",
-                    "ppm": sensor["ppm"]
-                }
+				ppm_data = {
+					"sensor_num": sensor["serial_number"],
+					"role": sensor["sensor_type"],
+					"type": "ppm",
+					"ppm": sensor["ppm"]
+				}
 
-                dispatch_sensor_data(ppm_data)
+				dispatch_sensor_data(ppm_data)
 
-            if sensor["sensor_type"] == "atlas_scientific_temp":
-                temp_data = {
-                    "sensor_num": sensor["serial_number"],
-                    "role": sensor["sensor_type"],
-                    "type": "hydro-temp",
-                    "hydro-temp": sensor["sensor_reading"]
-                }
+			if sensor["sensor_type"] == "atlas_scientific_temp":
+				temp_data = {
+					"sensor_num": sensor["serial_number"],
+					"role": sensor["sensor_type"],
+					"type": "hydro-temp",
+					"hydro-temp": sensor["sensor_reading"]
+				}
 
-                dispatch_sensor_data(temp_data)
+				dispatch_sensor_data(temp_data)
 
-            if sensor["sensor_type"] == "atlas_scientific_ph":
-                temp_data = {
-                    "sensor_num": sensor["serial_number"],
-                    "role": sensor["sensor_type"],
-                    "type": "hydro-ph",
-                    "hydro-ph": sensor["sensor_reading"]
-                }
+			if sensor["sensor_type"] == "atlas_scientific_ph":
+				temp_data = {
+					"sensor_num": sensor["serial_number"],
+					"role": sensor["sensor_type"],
+					"type": "hydro-ph",
+					"hydro-ph": sensor["sensor_reading"]
+				}
 
-                dispatch_sensor_data(temp_data)
+				dispatch_sensor_data(temp_data)
 
 
-while True:
-    format_data()
-    time.sleep(1000)
+
+format_data()
