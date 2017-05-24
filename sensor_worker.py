b97a0f9704d4ba454a08130ab031a06afce96c1a
rsstdd <rosstod@gmail.com>
2017-05-24 09:44:48 -0700

    Change to utc timestamp

--- sensor_worker.py
+++ sensor_worker.py
@@ -55,8 +55,8 @@ def dispatch_sensor_data(dataPackage):
 	if deviceData['location']['room'] in ['0804', '0808']:
 		postAPI('https://skagit-luna-api.herokuapp.com/sensordata', sensorRecord)
 	else:
-		postAPI('https://luna-api.herokuapp.com/sensordata', jsonPackage)
-		postAPI('https://luna-api-staging.herokuapp.com/sensordata', jsonPackage)
+		postAPI('https://luna-api.herokuapp.com/sensordata', sensorRecord)
+		postAPI('https://luna-api-staging.herokuapp.com/sensordata', sensorRecord)
 
 
 	#  Mongo
