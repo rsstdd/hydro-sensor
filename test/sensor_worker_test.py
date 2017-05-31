d54c687051aeb8e22221ca7c9318918e8876ce18
rsstdd <rosstod@gmail.com>
2017-05-31 09:44:04 -0700

    Testing

--- test/sensor_worker_test.py
+++ test/sensor_worker_test.py
@@ -3,6 +3,7 @@
 from sensor_worker2 import dispatch_sensor_data
 import nose
 from nose.tools import *
+import mock
 import unittest
 
 
@@ -11,9 +12,9 @@ class DispatchSensorDataTest(unittest.TestCase):
 
 	def dispatch_sensor_data_test(self):
 		"""It should format the JSON appropriately"""
-		json_string = {  "ppm":"618", "sensor_group":"Test", "role":"hydroTest", "type":"ppm", "ppm":"618" }
+		input_json = { "ppm":"618", "sensor_group":"Test", "role":"hydroTest", "type":"ppm", "ppm":"618" }
 		valid_json = "{'sensor_num':'DJ00RU96', sensordata': {'sensor_num': 'DJ00RU96', 'room': u'Grow Room 1', 'sensor_version': u'1.00', 'timestamp': datetime.datetime(2017, 5, 29, 20, 23, 53, 238618), 'hostname': u'iunuTestPi', 'ppm': '618', 'sensor_group': u'Test', 'role': u'hydroTest', 'type': 'ppm'}}"
 		result = dispatch_sensor_data(json_string)
-		nose.tools.ok_(result, valid_json)
+		assert result == valid_json
 
 result = nose.main()
